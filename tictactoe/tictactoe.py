165dbafd72143ad4968d631fb874a3792d78b91f
waltertorrestriana <walter.bim@hotmail.com>
2024-11-25 14:15:39 -0500

    Completes actions function.

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -13,16 +13,37 @@ def initial_state(): # S0
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, X, EMPTY],
-            [X, EMPTY, EMPTY],
-            [EMPTY, O, EMPTY]]
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
+    # Check if board is not over
+    if terminal(board):
+        return False
     
+    # Count number of occurrences for different possible values
+    x_counts = o_counts = empty_counts = 0  
+    for row in board:
+        for cell in row:
+            if cell == X:
+                x_counts += 1
+            elif cell == O:
+                o_counts +=1
+            elif cell == EMPTY:
+                empty_counts += 1
+    
+    # Return next turn based on counted elements logic
+    if empty_counts == 0:
+        return False
+    elif x_counts == o_counts:
+        return X
+    else:
+        return O
 
 
 def actions(board):
