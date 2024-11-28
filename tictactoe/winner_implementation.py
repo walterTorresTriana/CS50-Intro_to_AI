540eaab30dc2a14fe6dde4ce24b23bb02135bae7
waltertorrestriana <walter.bim@hotmail.com>
2024-11-28 08:33:35 -0500

    Adds minos changes.

--- tictactoe/winner_implementation.py
+++ tictactoe/winner_implementation.py
@@ -9,17 +9,17 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    # # Check rows
-    # for row in board:
-    #     if all(cell == row[0] for cell in row) and row[0] != EMPTY:
-    #         return row[0]
+    # Check rows
+    for row in board:
+        if all(cell == row[0] for cell in row) and row[0] != EMPTY:
+            return row[0]
         
-    # # Check columns
-    # for column in zip(*board):
-    #     if all(cell == column[0] for cell in column) and column[0] != EMPTY:
-    #         return column[0]        
+    # Check columns
+    for column in zip(*board):
+        if all(cell == column[0] for cell in column) and column[0] != EMPTY:
+            return column[0]        
     
-    # # Check diagonals
+    # Check diagonals
     diagonal = [board[i][i] for i in range(len(board))]
     antidiagonal = [board[i][len(board)-i-1] for i in range(len(board))]    
 
@@ -30,7 +30,7 @@ def winner(board):
         return antidiagonal[0]
 
     # If no winner, return None"
-    return antidiagonal
+    return None
 
 instance_of_board = initial_state()
 print(winner(instance_of_board))
