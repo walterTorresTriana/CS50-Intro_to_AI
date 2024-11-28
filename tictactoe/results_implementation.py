b19afea5f0d90ad8f00ef514d31340cc792b68ae
waltertorrestriana <walter.bim@hotmail.com>
2024-11-28 08:30:12 -0500

    Creates winner standalone and results function.

--- tictactoe/results_implementation.py
+++ tictactoe/results_implementation.py
@@ -1,23 +1,33 @@
 from tictactoe import initial_state
-from tictactoe import actions
+from tictactoe import actions, player, terminal
 
-# Temporary used while coding results function
-
+X = "X"
+O = "O"
+EMPTY = None
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    import copy
 
+    # Verify action is valid
+    if action not in actions(board):
+        raise ValueError("Invalid action.")
+    
+    # Verify game is not over yet.
+    if not terminal(board):
+        raise ValueError("Invalid action, the game is over.")
+    
+    # Create copy of the board
+    import copy
     new_board = copy.deepcopy(board)
 
-    l = []
-    for i in actions(instance_of_board):
-        if isinstance(i, tuple):
-            l.append("yes")
-    return l
+    # Make the move
+    new_board[action[0]][action[1]] = player(board)
 
+    return new_board
+
+# Temporary used while coding results function
 instance_of_board = initial_state()
 print(actions(instance_of_board))
 print(result(board=instance_of_board, action=actions(instance_of_board)))
