060a54f1ec52fe10072819bfe2b0b6da911171aa
waltertorrestriana <walter.bim@hotmail.com>
2024-11-28 09:00:26 -0500

    Adds terminal function.

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -116,6 +116,15 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    # Check if there is a winner
+    if winner(board):
+        return True
+    
+    # Check if there are empty spaces to make moves
+    if not any(EMPTY in row for row in board):
+        return True
+    
+    # Otherwise, the game is not over
     return False
 
 
